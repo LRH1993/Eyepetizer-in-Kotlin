9eca05666e09ba6b84e05112a3ed156a99181c00
lvruheng <lvruheng@bytedance.com>
2017-07-03 21:26:17 +0800

    启动页面实现

--- app/src/main/java/com/tt/lvruheng/eyepetizer/ui/SplashActivity.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/SplashActivity.kt
@@ -1,14 +1,69 @@
 package com.tt.lvruheng.eyepetizer.ui
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.view.animation.ScaleAnimation
+import android.widget.ImageView
+import android.widget.TextView
+import com.tt.lvruheng.eyepetizer.R
+import com.tt.lvruheng.eyepetizer.utils.newIntent
+import kotlinx.android.synthetic.main.activity_splash.*
 
 /**
  * Created by lvruheng on 2017/7/2.
  */
 class SplashActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    private lateinit var tv_name : TextView
+    private lateinit var tv_intro : TextView
+    private lateinit var image_bg :ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //设置全屏
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+        initView()
+
+
     }
+
+    private fun initView() {
+        tv_name = tv_name_english
+        tv_intro = tv_english_intro
+        image_bg = iv_icon_splash
+        val font :Typeface = Typeface.createFromAsset(this.assets,"fonts/Lobster-1.4.otf")
+        tv_name.typeface = font
+        tv_intro.typeface = font
+        val alphaAnimation = AlphaAnimation(0.1f,1.0f)
+        alphaAnimation.duration = 1000
+        val scaleAnimation = ScaleAnimation(0.1f,1.0f,0.1f,1.0f,ScaleAnimation.RELATIVE_TO_SELF,0.5f,ScaleAnimation.RELATIVE_TO_SELF,0.5f)
+        scaleAnimation.duration =1000
+        val animationSet = AnimationSet(true)
+        animationSet.addAnimation(alphaAnimation)
+        animationSet.addAnimation(scaleAnimation)
+        animationSet.duration = 1000
+        image_bg.startAnimation(animationSet)
+        animationSet.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                newIntent<MainActivity>()
+            }
+        })
+
+
+    }
+
+
 }
--- app/src/main/java/com/tt/lvruheng/eyepetizer/utils/Extension.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/utils/Extension.kt
@@ -1,6 +1,8 @@
 package com.tt.lvruheng.eyepetizer.utils
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
 
 /**
@@ -9,3 +11,12 @@ import android.widget.Toast
 fun Context.showToast(message: String) {
     Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
 }
+
+inline fun <reified T: Activity> Activity.newIntent() {
+    val intent = Intent(this, T::class.java)
+    startActivity(intent)
+}
+
+
+
+
