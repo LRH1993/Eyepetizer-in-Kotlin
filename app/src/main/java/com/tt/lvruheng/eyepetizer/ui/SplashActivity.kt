23f8d76196d3e266c53c643fe8ab197b46ccd50a
lvruheng <lvruheng@bytedance.com>
2017-07-04 10:26:33 +0800

    Activity跳转动画

--- app/src/main/java/com/tt/lvruheng/eyepetizer/ui/SplashActivity.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/SplashActivity.kt
@@ -19,26 +19,18 @@ import kotlinx.android.synthetic.main.activity_splash.*
  * Created by lvruheng on 2017/7/2.
  */
 class SplashActivity : AppCompatActivity() {
-    private lateinit var tv_name : TextView
-    private lateinit var tv_intro : TextView
-    private lateinit var image_bg :ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //设置全屏
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
         initView()
+        setAnimation()
 
 
     }
 
-    private fun initView() {
-        tv_name = tv_name_english
-        tv_intro = tv_english_intro
-        image_bg = iv_icon_splash
-        val font :Typeface = Typeface.createFromAsset(this.assets,"fonts/Lobster-1.4.otf")
-        tv_name.typeface = font
-        tv_intro.typeface = font
+    private fun setAnimation() {
         val alphaAnimation = AlphaAnimation(0.1f,1.0f)
         alphaAnimation.duration = 1000
         val scaleAnimation = ScaleAnimation(0.1f,1.0f,0.1f,1.0f,ScaleAnimation.RELATIVE_TO_SELF,0.5f,ScaleAnimation.RELATIVE_TO_SELF,0.5f)
@@ -47,7 +39,7 @@ class SplashActivity : AppCompatActivity() {
         animationSet.addAnimation(alphaAnimation)
         animationSet.addAnimation(scaleAnimation)
         animationSet.duration = 1000
-        image_bg.startAnimation(animationSet)
+        iv_icon_splash.startAnimation(animationSet)
         animationSet.setAnimationListener(object : Animation.AnimationListener{
             override fun onAnimationRepeat(animation: Animation?) {
 
@@ -59,10 +51,15 @@ class SplashActivity : AppCompatActivity() {
 
             override fun onAnimationEnd(animation: Animation?) {
                 newIntent<MainActivity>()
+                finish()
             }
         })
+    }
 
-
+    private fun initView() {
+        val font :Typeface = Typeface.createFromAsset(this.assets,"fonts/Lobster-1.4.otf")
+        tv_name_english.typeface = font
+        tv_english_intro.typeface = font
     }
 
 
