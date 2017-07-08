88885971912b0500d91ced884945df9caa18641e
lvruheng <lvruheng@bytedance.com>
2017-07-08 09:22:15 +0800

    视频播放逻辑

--- app/src/main/java/com/tt/lvruheng/eyepetizer/ui/VideoDetailActivity.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/VideoDetailActivity.kt
@@ -1,5 +1,8 @@
 package com.tt.lvruheng.eyepetizer.ui
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -16,6 +19,10 @@ import android.os.Message
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import com.shuyu.gsyvideoplayer.GSYVideoPlayer
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils
+import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer
+import com.tt.lvruheng.eyepetizer.utils.VideoListener
 import java.io.FileInputStream
 import java.io.FileNotFoundException
 import java.util.concurrent.ExecutionException
@@ -29,14 +36,18 @@ class VideoDetailActivity : AppCompatActivity() {
         var MSG_IMAGE_LOADED = 101
     }
 
+    var mContext: Context = this
     lateinit var imageView: ImageView
     lateinit var bean: VideoBean
+    var isPlay: Boolean = false
+    var isPause: Boolean = false
+    lateinit var orientationUtils: OrientationUtils
     var mHandler: Handler = object : Handler() {
         override fun handleMessage(msg: Message?) {
             super.handleMessage(msg)
             when (msg?.what) {
-                MSG_IMAGE_LOADED ->{
-                    Log.e("video","setImage")
+                MSG_IMAGE_LOADED -> {
+                    Log.e("video", "setImage")
                     gsy_player.setThumbImageView(imageView)
                 }
 
@@ -47,7 +58,6 @@ class VideoDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ImmersionBar.with(this).statusBarColor(R.color.black).fitsSystemWindows(true).init()
         setContentView(R.layout.activity_video_detail)
         bean = intent.getParcelableExtra<VideoBean>("data")
         initView()
@@ -84,13 +94,50 @@ class VideoDetailActivity : AppCompatActivity() {
     }
 
     private fun prepareVideo() {
-        gsy_player.setUp(bean.playUrl,false,null, null)
+        gsy_player.setUp(bean.playUrl, false, null, null)
         //增加封面
         imageView = ImageView(this)
         imageView.scaleType = ImageView.ScaleType.CENTER_CROP
         ImageViewAsyncTask(mHandler, this, imageView).execute(bean.feed)
         gsy_player.titleTextView.visibility = View.GONE
-        gsy_player.backButton.visibility = View.VISIBLE
+        gsy_player.backButton.visibility = View.GONE
+        orientationUtils = OrientationUtils(this, gsy_player)
+        gsy_player.setIsTouchWiget(true);
+        //关闭自动旋转
+        gsy_player.isRotateViewAuto = false;
+        gsy_player.isLockLand = false;
+        gsy_player.isShowFullAnimation = false;
+        gsy_player.isNeedLockFull = true;
+        gsy_player.fullscreenButton.setOnClickListener {
+            //直接横屏
+            orientationUtils.resolveByClick();
+            //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+            gsy_player.startWindowFullscreen(mContext, true, true);
+        }
+        gsy_player.setStandardVideoAllCallBack(object : VideoListener() {
+            override fun onPrepared(url: String?, vararg objects: Any?) {
+                super.onPrepared(url, *objects)
+                //开始播放了才能旋转和全屏
+                orientationUtils.isEnable = true
+                isPlay = true;
+            }
+            override fun onAutoComplete(url: String?, vararg objects: Any?) {
+                super.onAutoComplete(url, *objects)
+
+            }
+            override fun onClickStartError(url: String?, vararg objects: Any?) {
+                super.onClickStartError(url, *objects)
+            }
+            override fun onQuitFullscreen(url: String?, vararg objects: Any?) {
+                super.onQuitFullscreen(url, *objects)
+                orientationUtils?.let { orientationUtils.backToProtVideo() }
+            }
+        })
+        gsy_player.setLockClickListener { view, lock ->
+            //配合下方的onConfigurationChanged
+            orientationUtils.isEnable = !lock
+        }
+
     }
 
     private class ImageViewAsyncTask(handler: Handler, activity: VideoDetailActivity, private val mImageView: ImageView) : AsyncTask<String, Void, String>() {
@@ -126,6 +173,51 @@ class VideoDetailActivity : AppCompatActivity() {
             var message = handler.obtainMessage()
             message.what = MSG_IMAGE_LOADED
             handler.sendMessage(message)
+        }
+    }
+
+    override fun onBackPressed() {
+        orientationUtils?.let {
+            orientationUtils.backToProtVideo()
+        }
+        if (StandardGSYVideoPlayer.backFromWindowFull(this)) {
+            return
+        }
+        super.onBackPressed()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        isPause = true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isPause = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GSYVideoPlayer.releaseAllVideos()
+        orientationUtils?.let {
+            orientationUtils.releaseListener()
+        }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        if (isPlay && !isPause) {
+            if (newConfig?.orientation == ActivityInfo.SCREEN_ORIENTATION_USER) {
+                if (!gsy_player.isIfCurrentIsFullscreen) {
+                    gsy_player.startWindowFullscreen(mContext, true, true)
+                }
+            } else {
+                //新版本isIfCurrentIsFullscreen的标志位内部提前设置了，所以不会和手动点击冲突
+                if (gsy_player.isIfCurrentIsFullscreen) {
+                    StandardGSYVideoPlayer.backFromWindowFull(this);
+                }
+                orientationUtils?.let { orientationUtils.isEnable = true }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/utils/VideoListener.kt
@@ -0,0 +1,73 @@
+package com.tt.lvruheng.eyepetizer.utils
+
+import com.shuyu.gsyvideoplayer.listener.StandardVideoAllCallBack
+
+/**
+ * Created by lvruheng on 2017/7/8.
+ */
+open class VideoListener : StandardVideoAllCallBack{
+    override fun onClickResumeFullscreen(url: String?, vararg objects: Any?) {
+
+    }
+
+    override fun onEnterFullscreen(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickResume(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickSeekbarFullscreen(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickBlankFullscreen(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onPrepared(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickStartIcon(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onAutoComplete(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onQuitSmallWidget(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onTouchScreenSeekVolume(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickBlank(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickStop(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onTouchScreenSeekLight(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickSeekbar(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onQuitFullscreen(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onPlayError(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickStartThumb(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onEnterSmallWidget(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickStopFullscreen(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onClickStartError(url: String?, vararg objects: Any?) {
+    }
+
+    override fun onTouchScreenSeekPosition(url: String?, vararg objects: Any?) {
+    }
+
+}
