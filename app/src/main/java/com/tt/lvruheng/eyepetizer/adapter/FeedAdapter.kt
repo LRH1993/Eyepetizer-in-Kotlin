ba08d9815c4b2b66da2c4ca2172b99f444529b36
lvruheng <lvruheng@bytedance.com>
2017-07-11 14:04:11 +0800

    搜索结果页实现

---
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/adapter/FeedAdapter.kt
@@ -0,0 +1,91 @@
+package com.tt.lvruheng.eyepetizer.adapter
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Typeface
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.tt.lvruheng.eyepetizer.R
+import com.tt.lvruheng.eyepetizer.mvp.model.bean.HotBean
+import com.tt.lvruheng.eyepetizer.mvp.model.bean.VideoBean
+import com.tt.lvruheng.eyepetizer.ui.VideoDetailActivity
+import com.tt.lvruheng.eyepetizer.utils.ImageLoadUtils
+import java.text.SimpleDateFormat
+
+/**
+ * Created by lvruheng on 2017/7/7.
+ */
+class FeedAdapter(context: Context, list: ArrayList<HotBean.ItemListBean.DataBean>) : RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
+    var context: Context? = null;
+    var list: ArrayList<HotBean.ItemListBean.DataBean>? = null
+    var inflater: LayoutInflater? = null
+
+    init {
+        this.context = context
+        this.list = list
+        this.inflater = LayoutInflater.from(context)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FeedViewHolder {
+        return FeedViewHolder(inflater?.inflate(R.layout.item_feed_result, parent, false), context!!)
+    }
+
+    override fun getItemCount(): Int {
+        return list?.size ?: 0
+    }
+
+    override fun onBindViewHolder(holder: FeedViewHolder?, position: Int) {
+        var photoUrl : String? = list?.get(position)?.cover?.feed
+        photoUrl?.let { ImageLoadUtils.display(context!!,holder?.iv_photo, it) }
+        var title : String? = list?.get(position)?.title
+        holder?.tv_title?.text = title
+        var category = list?.get(position)?.category
+        var duration = list?.get(position)?.duration
+        var minute =duration?.div(60)
+        var second = duration?.minus((minute?.times(60)) as Long )
+        var releaseTime = list?.get(position)?.releaseTime
+        var smf : SimpleDateFormat = SimpleDateFormat("MM-dd")
+        var date = smf.format(releaseTime)
+        var realMinute : String
+        var realSecond : String
+        if(minute!!<10){
+            realMinute = "0"+minute
+        }else{
+            realMinute = minute.toString()
+        }
+        if(second!!<10){
+            realSecond = "0"+second
+        }else{
+            realSecond = second.toString()
+        }
+        holder?.tv_time?.text = "$category / $realMinute'$realSecond'' / $date"
+        holder?.itemView?.setOnClickListener {
+            //跳转视频详情页
+            var intent : Intent = Intent(context, VideoDetailActivity::class.java)
+            var desc = list?.get(position)?.description
+            var playUrl = list?.get(position)?.playUrl
+            var blurred = list?.get(position)?.cover?.blurred
+            var collect = list?.get(position)?.consumption?.collectionCount
+            var share = list?.get(position)?.consumption?.shareCount
+            var reply = list?.get(position)?.consumption?.replyCount
+            var videoBean  = VideoBean(photoUrl,title,desc,duration,playUrl,category,blurred,collect ,share ,reply)
+            intent.putExtra("data",videoBean)
+            context?.let { context -> context.startActivity(intent) }
+        }
+    }
+
+
+    class FeedViewHolder(itemView: View?, context: Context) : RecyclerView.ViewHolder(itemView) {
+        var iv_photo: ImageView = itemView?.findViewById(R.id.iv_photo) as ImageView
+        var tv_title: TextView = itemView?.findViewById(R.id.tv_title) as TextView
+        var tv_time: TextView = itemView?.findViewById(R.id.tv_detail) as TextView
+        init {
+            tv_title?.typeface = Typeface.createFromAsset(context?.assets, "fonts/FZLanTingHeiS-L-GB-Regular.TTF")
+
+        }
+    }
+}
--- app/src/main/java/com/tt/lvruheng/eyepetizer/ui/ResultActivity.kt
+++ app/src/main/java/com/tt/lvruheng/eyepetizer/ui/ResultActivity.kt
@@ -1,30 +1,85 @@
 package com.tt.lvruheng.eyepetizer.ui
 
-import android.icu.lang.UCharacter.GraphemeClusterBreak.L
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import com.gyf.barlibrary.ImmersionBar
 import com.tt.lvruheng.eyepetizer.R
+import com.tt.lvruheng.eyepetizer.adapter.FeedAdapter
 import com.tt.lvruheng.eyepetizer.mvp.contract.ResultContract
 import com.tt.lvruheng.eyepetizer.mvp.model.bean.HotBean
 import com.tt.lvruheng.eyepetizer.mvp.presenter.ResultPresenter
+import kotlinx.android.synthetic.main.activity_find_detail.*
+import kotlin.collections.ArrayList
 
 /**
  * Created by lvruheng on 2017/7/11.
  */
-class ResultActivity : AppCompatActivity(),ResultContract.View {
+class ResultActivity : AppCompatActivity(), ResultContract.View, SwipeRefreshLayout.OnRefreshListener {
     lateinit var keyWord: String
     lateinit var mPresenter: ResultPresenter
-    var start : Int = 10
+    lateinit var mAdapter: FeedAdapter
+    var mIsRefresh: Boolean = false
+    var mList = ArrayList<HotBean.ItemListBean.DataBean>()
+    var start: Int = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ImmersionBar.with(this).transparentBar().barAlpha(0.3f).fitsSystemWindows(true).init()
         setContentView(R.layout.activity_result)
         keyWord = intent.getStringExtra("keyWord")
-        mPresenter = ResultPresenter(this,this)
-        mPresenter.requestData(keyWord,start)
+        mPresenter = ResultPresenter(this, this)
+        mPresenter.requestData(keyWord, start)
+        setToolbar()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        mAdapter = FeedAdapter(this, mList)
+        recyclerView.adapter = mAdapter
+        refreshLayout.setOnRefreshListener(this)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                var layoutManager: LinearLayoutManager = recyclerView?.layoutManager as LinearLayoutManager
+                var lastPositon = layoutManager.findLastVisibleItemPosition()
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && lastPositon == mList.size - 1) {
+                    start = start.plus(10)
+                    mPresenter.requestData(keyWord, start)
+                }
+            }
+        })
     }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        var bar = supportActionBar
+        bar?.title = "'$keyWord' 相关"
+        bar?.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+
     override fun setData(bean: HotBean) {
+        if (mIsRefresh) {
+            mIsRefresh = false
+            refreshLayout.isRefreshing = false
+            if (mList.size > 0) {
+                mList.clear()
+            }
+
+        }
+        bean.itemList?.forEach {
+            it.data?.let { it1 -> mList.add(it1) }
+        }
+        mAdapter.notifyDataSetChanged()
+    }
+
+    override fun onRefresh() {
+        if (!mIsRefresh) {
+            mIsRefresh = true
+            start = 10
+            mPresenter.requestData(keyWord, start)
+        }
     }
 }
